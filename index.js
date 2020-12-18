9aeb37d24d9351d5964c965a5269674117e026d7
junyoung8753 <junyoung8753@gmail.com>
2020-12-18 15:01:33 +0900

    first upload

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-console.log("Im Working. Im JS. Im Beautiful and SEXY.");
 
+
+const junyoungInfo = {
+    name: "Junyoung",
+    age: 20,
+    gender: "Male",
+    isHandsome: true,
+    favMovies: ["motherfucker", "suckMyDick", "sexSex"],
+    favFood: [
+      {
+        name: "Kimchi",
+        fatty: false
+      }, 
+      {
+        name: "Cheese burger",
+        fatty: true
+      }
+    ]
+  };
+  
+  
+  // console.log(junyoungInfo.favFood[0].name);
+  
+  
+  const avMovies= ["motherfucker", "suckMyDick", "sexSex"];
+  
+  
+  
+  
+  
+  console.log(junyoungInfo.favMovies, console.log, console);
