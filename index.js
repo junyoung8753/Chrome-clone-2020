5201c545759da3857ed0f9fed1c872b07a073126
junyoung8753 <junyoung8753@gmail.com>
2020-12-19 22:52:39 +0900

    js change

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
+/* const calculator = {
+  plus: function(a, b){return a + b},
+  minus: function(a, b){return a - b},
+  multiple: function(a, b){return a * b},
+  divide: function(a, b){return a / b},
+  powerOf: function(a, b){return a ** b}
+  } ;
+
+const duhagi = calculator.plus(1,2);
+const ffegi = calculator.minus(2,3);
+const gopagi = calculator.multiple(4,3);
+const nanugi = calculator.divide(8,2);
+const seungsu = calculator.powerOf(2,3);
+
+console.log(duhagi);
+console.log(ffegi);
+console.log(gopagi);
+console.log(nanugi);
+console.log(seungsu); */
 
 
-const junyoungInfo = {
-    name: "Junyoung",
-    age: 20,
-    gender: "Male",
-    isHandsome: true,
-    favMovies: ["motherfucker", "suckMyDick", "sexSex"],
-    favFood: [
-      {
-        name: "Kimchi",
-        fatty: false
-      }, 
-      {
-        name: "Cheese burger",
-        fatty: true
-      }
-    ]
-  };
-  
-  
-  // console.log(junyoungInfo.favFood[0].name);
-  
-  
-  const avMovies= ["motherfucker", "suckMyDick", "sexSex"];
-  
-  
-  
-  
-  
-  console.log(junyoungInfo.favMovies, console.log, console);
+
+
