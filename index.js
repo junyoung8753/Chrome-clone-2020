0fad6bc95f8ed16726868eaabdc57b45607a51f9
junyoung8753 <junyoung8753@Gmail.com>
2020-12-19 13:43:45 +0000

    update study

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
+ const calculator = {
+  plus: function(a, b){
+    return a + b
+    };
+  minus: function(a, b){
+    return a - b; 
+    };
+  divide: function(a, b){
+    return a / b;
+    };
+  multiple: function(a, b){
+    return a * b; 
+    };
+  } ;
 
-
-const junyoungInfo = {
-    name: "Junyoung",
-    age: 20,
-    gender: "Male",
-    isHandsome: true,
-    favMovies: ["motherfucker", "suckMyDick", "sexSex"],
-    favFood: [
-      {
-        name: "Kimchi",
-        fatty: false
-      }, 
-      {
-        name: "Cheese burger",
-        fatty: true
-      }
-    ]
-  };
-  
-  
-  // console.log(junyoungInfo.favFood[0].name);
-  
-  
-  const avMovies= ["motherfucker", "suckMyDick", "sexSex"];
-  
-  
-  
-  
-  
-  console.log(junyoungInfo.favMovies, console.log, console);
+calculator.plus(1,2);
